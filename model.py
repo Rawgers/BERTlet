7e056cba641d2e2de95fa8961d639753355125dd
Tommy Wei <tommywei@google.com>
2021-03-28 13:57:12 -0700

    model v1

--- model.py
+++ model.py
@@ -10,19 +10,20 @@ class LanguageModel(nn.Module):
         super().__init__()
         self.use_residue = use_residue
         self.bert = BertModel.from_pretrained(bert_model_link)
-        self.lstm = nn.LSTM(self.bert.hidden_size, lstm_hidden_size
+        self.lstm = nn.LSTM(self.bert.config.hidden_size, lstm_hidden_size,
                 batch_first=batch_first, dropout=dropout)
         self.dropout = nn.Dropout(dropout)
-        linear_input_shape = lstm_hidden_size + self.bert.hidden_size if \
+        linear_input_shape = lstm_hidden_size + self.bert.config.hidden_size if \
                 use_residue else lstm_hidden_size
 
         self.linear = nn.Linear(linear_input_shape, output_shape)
 
     def forward(self,x):
         bert_output = self.bert(**x)
-        lstm_output = self.lstm(bert_output.last_hidden_state)
+        lstm_output,_ = self.lstm(bert_output.last_hidden_state)
         dropout_output = self.dropout(lstm_output)
-        input_to_linear = th.vstach((dropout_output, bert_output.pooler_output)) if self.use_residue else dropout_output
+        last_dropout = dropout_output[:, -1, :].squeeze()
+        input_to_linear = th.hstack((last_dropout, bert_output.pooler_output)) if self.use_residue else last_dropout
         linear_output = self.linear(input_to_linear)
         return linear_output
 
